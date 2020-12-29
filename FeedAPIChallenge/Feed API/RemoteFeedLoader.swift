a803b698d086194d9da687b5f1062dbd5f325a51
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-12-29 14:45:47 -0500

    Guarantee that load delivers Invalid data on Non 200 HTTP Response by adding a completion to the load method implementing the success and failure result.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,8 +19,13 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { _ in
-			completion(.failure(Error.connectivity))
+		client.get(from: url) { result in
+			switch result {
+			case .success((_,_)):
+				completion(.failure(Error.invalidData))
+			case .failure(_):
+				completion(.failure(Error.connectivity))
+			}
 		}
 	}
 }
